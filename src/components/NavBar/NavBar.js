edbedbb251a11591085eb5f619768e43c4186cd2
Stephen Allen <staylorallen@gmail.com>
2019-11-14 08:15:05 -0500

    main header

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavBar.css';
+
+const navBar = (props) => {
+  return (
+    <div className = "header">
+      <h1>
+        <a>Stephen Allen</a>
+      </h1>
+      <nav>Menu</nav>
+    </div>
+  )
+}
+
+export default navBar;
